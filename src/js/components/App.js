eb9ffd6148d5c83a6be8a67be18e310b5bc6a962
Christine Movius <cmovius@ord.blueraster.com>
2016-05-15 21:39:23 -0400

    Add boilerplate to-do list layout.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -21,6 +21,15 @@ export default class App extends Component {
   render() {
     return(
       <div className='page-content'>
+        <h1>To Do List</h1>
+        <input type='text' placeholder='New Task'/>
+        <button>Add</button>
+        <h2>Tasks</h2>
+        <ul>
+          <li>Task 1<input type ='checkbox'/></li>
+          <li>Task 2<input type ='checkbox'/></li>
+        </ul>
+        <button>Delete Completed Tasks</button>
         <h1>{this.state.title}</h1>
         <button onClick={this.setTitle}>Click</button>
       </div>
